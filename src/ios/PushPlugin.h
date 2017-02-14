188d08cf769ef1f2e7d063d6a180801d9f309779
Mohamad Raziff Azhar <raziff@Mohamads-MacBook-Pro.local>
2017-02-14 13:56:01 +0800

    added registerPushEcho in PushPlugin.h

--- src/ios/PushPlugin.h
+++ src/ios/PushPlugin.h
@@ -57,6 +57,7 @@
 - (void)unregister:(CDVInvokedUrlCommand*)command;
 - (void)subscribe:(CDVInvokedUrlCommand*)command;
 - (void)unsubscribe:(CDVInvokedUrlCommand*)command;
+- (void)registerPushEcho:(CDVInvokedUrlCommand*)command;
 
 - (void)didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken;
 - (void)didFailToRegisterForRemoteNotificationsWithError:(NSError *)error;
